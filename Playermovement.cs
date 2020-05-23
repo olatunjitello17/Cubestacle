deda261d7f629708b2448b0aa6eb0d8b9dd3dede
Tellobaba <61257398+olatunjitello17@users.noreply.github.com>
2020-05-23 10:57:47 +0300

    Add files via upload

---
+++ Playermovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+using UnityEngine;
+
+public class Playermovement : MonoBehaviour {
+
+
+    public Rigidbody rb;
+
+    public float forwardforce = 2000f;
+
+
+    void FixedUpdate()
+    {
+
+        rb.AddForce(0, 0, forwardforce * Time.deltaTime);
+
+        if (Input.GetKey("d") )
+        {
+            rb.AddForce(500 * Time.deltaTime, 0, 0);
+
+
+
+
+
+        }
+
+   
+       
+    }
+ }
